ae77adc539d7734562c170e5716192481bbd40cf
Evgenii Nasyrov <inasyrov@ya.ru>
2019-07-31 19:53:43 +0100

    refactor: refactor to composer plugin (#2)
    * Convert to composer plugin
    
    * Remove bootstrap
    
    * Refactor command to composer plugin

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Require Composer autoloader if installed on it's own
-if (file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
-    require_once $composer;
-}
-
-// Register command
-\Helick\MUPluginsDiscovery\Command::register();
